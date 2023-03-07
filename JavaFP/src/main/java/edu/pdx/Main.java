b8a05627998733748ceba0dc94bbb5b4c2fe3150
Yao Li <hnkfliyao@gmail.com>
2023-03-07 00:11:26 -0800

    Java.

---
+++ JavaFP/src/main/java/edu/pdx/Main.java
@@ -0,0 +1,12 @@
+package edu.pdx;
+
+import java.util.stream.Stream;
+
+public class Main
+{
+    public static void main(String[] args) {
+        Stream<Integer> xs = Stream.of(1, 2, 3, 4, 5);
+        xs.map((i) -> i * 2);
+        xs.forEach((s) -> { System.out.println(s); });
+    }
+}
