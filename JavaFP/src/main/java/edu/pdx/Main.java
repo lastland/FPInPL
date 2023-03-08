9c98e467c7ddd0a5bfc99edc706db8e098d42073
Yao Li <hnkfliyao@gmail.com>
2023-03-08 14:20:46 -0800

    Using visitor.

--- JavaFP/src/main/java/edu/pdx/Main.java
+++ JavaFP/src/main/java/edu/pdx/Main.java
@@ -7,5 +7,13 @@ public class Main
     public static void main(String[] args) {
         Stream<Integer> xs = Stream.of(1, 2, 3, 4, 5);
         xs.map((i) -> i * 2 ).forEach((s) -> { System.out.println(s); });
+
+        Nat zero = NatFactory.Z();
+        Nat one = NatFactory.S(zero);
+        Nat two = NatFactory.S(one);
+
+        NatAddVisitor v = new NatAddVisitor();
+        System.out.println(zero.accept(v).apply(two)); // Expecting two
+        System.out.println(two.accept(v).apply(two));  // Expecting four
     }
 }
